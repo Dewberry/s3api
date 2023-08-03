91e4bee79386fcbb33830cf8224b4c2c524b9749
Seth Lawler <slawler@dewberry.com>
2023-08-03 13:56:26 -0400

    add boilerplate from modelviewer

---
+++ config/api.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"app/blobstore"
+	"fmt"
+	"os"
+)
+
+const CSI string = `
+------------------------------------------------------------------------
+    ____               __                               ___________ ____
+   / __ \___ _      __/ /_  ___  ____________  __      / ____/ ___//  _/
+  / / / / _ \ | /| / / __ \/ _ \/ ___/ ___/ / / /_____/ /    \__ \ / /
+ / /_/ /  __/ |/ |/ / /_/ /  __/ /  / /  / /_/ /_____/ /___ ___/ // /
+/_____/\___/|__/|__/_.___/\___/_/  /_/   \__, /      \____//____/___/
+                                        /____/
+-----------------------------------------------------------------------`
+
+type APIConfig struct {
+	Name    string
+	Port    string
+	Host    string
+	Address string
+	Build   string
+	BH      *blobstore.BlobHandler
+}
+
+func Init() *APIConfig {
+	return newAPI()
+}
+
+func newAPI() *APIConfig {
+	bh := blobstore.NewBlobHandler()
+
+	api := new(APIConfig)
+	api.Name = SERVICE_NAME
+	api.Port = SERVICE_PORT
+	api.Host = DEFAULT_HOST
+	api.Address = fmt.Sprintf("%s:%s", DEFAULT_HOST, SERVICE_PORT)
+	api.Build = os.Getenv("BUILD_TYPE")
+	api.BH = bh
+	return api
+}
---
+++ config/settings.go
@@ -0,0 +1,7 @@
+package config
+
+const (
+	SERVICE_NAME string = "/"
+	SERVICE_PORT string = "5000"
+	DEFAULT_HOST string = "" // 0.0.0.0
+)
