991d755889dad802b3d8618027162868f2092b5d
Anton Kopti <akopti@dewberry.com>
2023-08-04 16:25:39 +0000

    organize main.go

--- main.go
+++ main.go
@@ -28,22 +28,30 @@ func main() {
 		AllowOrigins:     []string{"*"},
 	}))
 
+	//health check
 	e.GET("/ping", bh.Ping)
+	//bucket endpoints
 	e.GET("/s3/buckets", auth.Authorize(bh.HandleListBuckets, allUsers...))
 	e.POST("/s3/buckets", auth.Authorize(bh.HandleCreateBucket, admin...))
 	e.DELETE("/s3/buckets", auth.Authorize(bh.HandleDeleteBucket, admin...))
 	e.GET("/s3/buckets/acl", auth.Authorize(bh.HandleGetBucketACL, admin...))
-	e.PUT("/s3/object/rename", auth.Authorize(bh.HandleRenameObject, writer...))
-	e.GET("/s3/list", auth.Authorize(bh.HandleListByPrefix, allUsers...))
-	e.GET("/s3/get_size", auth.Authorize(bh.HandleGetSize, allUsers...))
+	//object/s metadata
 	e.GET("/s3/get_metadata", auth.Authorize(bh.HandleGetMetaData, allUsers...))
+	e.PUT("/s3/object/rename", auth.Authorize(bh.HandleRenameObject, writer...))
+	e.GET("/s3/get_size", auth.Authorize(bh.HandleGetSize, allUsers...))
+	//object content
+	e.GET("/s3/file_contents", auth.Authorize(bh.HandleObjectContents, allUsers...))
+	//listing
+	e.GET("/s3/list", auth.Authorize(bh.HandleListByPrefix, allUsers...))
+	e.GET("/s3/bucket_view_list", auth.Authorize(bh.HandleBucketViewList, allUsers...))
+	//downloading
 	e.GET("/s3/download", auth.Authorize(bh.HandleGetPresignedURL, allUsers...))
 	e.GET("/s3/download_folder", auth.Authorize(bh.HandleGetPresignedURLMultiObj, allUsers...))
+	//uploading
 	e.POST("/s3/stream_upload", auth.Authorize(bh.HandleMultipartUpload, writer...))
+	//deleting objects
 	e.DELETE("/s3/delete", auth.Authorize(bh.HandleDeleteObjects, admin...))
 	e.DELETE("/s3/delete/list", auth.Authorize(bh.HandleDeleteObjectsByList, admin...))
-	e.GET("/s3/file_contents", auth.Authorize(bh.HandleObjectContents, allUsers...))
-	e.GET("/s3/bucket_view_list", auth.Authorize(bh.HandleBucketViewList, allUsers...))
 
 	e.Logger.Fatal(e.Start(":" + apiConfig.Port))
 	e.Logger.SetLevel(log.DEBUG)
