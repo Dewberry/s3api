5c44b6e26eb4c3ee0e0ae43af521f0b72f405c34
Anton Kopti <akopti@dewberry.com>
2023-08-04 02:32:39 +0000

    seperate functionality to different files

---
+++ blobstore/metadata.go
@@ -0,0 +1,99 @@
+package blobstore
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+)
+
+// HandleGetSize retrieves the total size and the number of files in the specified S3 bucket with the given prefix.
+func (bh *BlobHandler) HandleGetSize(c echo.Context) error {
+	prefix := c.QueryParam("prefix")
+	if prefix == "" {
+		err := errors.New("request must include a `prefix` parameter")
+		log.Info("HandleGetSize: " + err.Error())
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	bucket := c.QueryParam("bucket")
+	if bucket == "" {
+		if os.Getenv("S3_BUCKET") == "" {
+			err := errors.New("error: `bucket` parameter was not provided by the user and is not a global env variable")
+			log.Info("HandleGetSize: " + err.Error())
+			return c.JSON(http.StatusInternalServerError, err.Error())
+		}
+		bucket = os.Getenv("S3_BUCKET")
+	}
+	list, err := bh.getList(bucket, prefix, false)
+	if err != nil {
+		log.Info("HandleGetSize: Error getting list:", err.Error())
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	size, fileCount, err := bh.getSize(list)
+	if err != nil {
+		log.Info("HandleGetSize: Error getting size:", err.Error())
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	response := struct {
+		Size      uint64 `json:"size"`
+		FileCount uint32 `json:"file_count"`
+		Prefix    string `json:"prefix"`
+	}{
+		Size:      size,
+		FileCount: fileCount,
+		Prefix:    prefix,
+	}
+
+	log.Debug("HandleGetSize: Successfully retrieved size for prefix:", prefix)
+	return c.JSON(http.StatusOK, response)
+}
+
+// HandleGetMetaData retrieves the metadata of an object from the specified S3 bucket.
+// It takes the object key as a parameter and returns the metadata or an error as a JSON response.
+//
+// The key parameter represents the key of the object in the S3 bucket.
+// If the key ends with '/', it indicates a prefix instead of an object key and returns an error.
+// The S3_BUCKET environment variable is used as the bucket name.
+func (bh *BlobHandler) HandleGetMetaData(c echo.Context) error {
+	key := c.QueryParam("key")
+	if key == "" {
+		err := errors.New("request must include a `key` parameter")
+		log.Info("HandleGetMetaData: " + err.Error())
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	bucket := c.QueryParam("bucket")
+	if bucket == "" {
+		if os.Getenv("S3_BUCKET") == "" {
+			err := errors.New("error: `bucket` parameter  was not provided by the user and is not defined in .env")
+			log.Info("HandleGetMetaData: " + err.Error())
+			return c.JSON(http.StatusInternalServerError, err.Error())
+		}
+		bucket = os.Getenv("S3_BUCKET")
+	}
+	// Set up the input parameters for the list objects operation
+	input := &s3.HeadObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	}
+
+	result, err := bh.S3Svc.HeadObject(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok && aerr.Code() == "NotFound" {
+			err := fmt.Errorf("object %s not found", key)
+			log.Info("HandleGetMetaData: " + err.Error())
+			return c.JSON(http.StatusBadRequest, err.Error())
+		}
+		log.Info("HandleGetMetaData: Error getting metadata:", err.Error())
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	log.Info("HandleGetMetaData: Successfully retrieved metadata for key:", key)
+	return c.JSON(http.StatusOK, result)
+}
