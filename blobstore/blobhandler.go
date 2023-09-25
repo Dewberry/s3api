6c656529b3b38fc328e103f6c3d223dced592cf1
Anton Kopti <akopti@dewberry.com>
2023-09-25 11:10:46 -0400

    add error return in blobhandler creation

--- blobstore/blobhandler.go
+++ blobstore/blobhandler.go
@@ -1,6 +1,8 @@
 package blobstore
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
 	"os"
 
@@ -20,22 +22,28 @@ type BlobHandler struct {
 
 // Initializes resources and return a new handler
 // errors are fatal
-func NewBlobHandler() *BlobHandler {
-	// working with pointers here so as not to copy large templates, yamls, and ActiveJobs
+func NewBlobHandler() (*BlobHandler, error) {
 	config := BlobHandler{}
 
 	// Set up a session with AWS credentials and region
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
+	sess, err := session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
-	}))
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create AWS session: %v", err)
+	}
+
 	config.S3Svc = s3.New(sess)
 	config.Sess = sess
-	if(os.Getenv("S3_BUCKET")==""){
-		log.Fatal("S3_BUCKET environment variable is not set")
+
+	if os.Getenv("S3_BUCKET") == "" {
+		return nil, errors.New("S3_BUCKET environment variable is not set")
 	}
 	config.Bucket = os.Getenv("S3_BUCKET")
-	return &config
+
+	return &config, nil
 }
+
 func (bh *BlobHandler) Ping(c echo.Context) error {
 	return c.JSON(http.StatusOK, "connection without Auth is healthy")
 }
@@ -51,4 +59,3 @@ func (bh *BlobHandler) PingWithAuth(c echo.Context) error {
 	log.Infof("Ping operation preformed succesfully, connection to `%s` is healthy", bh.Bucket)
 	return c.JSON(http.StatusOK, "connection is healthy to s3 bucket")
 }
-
