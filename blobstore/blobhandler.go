fd62a0c8b5605af7de7feeb654581b23d6facfb4
Anton Kopti <akopti@dewberry.com>
2023-09-27 12:30:34 -0400

    add minio set up

--- blobstore/blobhandler.go
+++ blobstore/blobhandler.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/labstack/echo/v4"
@@ -26,14 +27,12 @@ func NewBlobHandler() (*BlobHandler, error) {
 	config := BlobHandler{}
 
 	// Set up a session with AWS credentials and region
-	sess, err := session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	})
+	s3SVC, sess, err := SessionManager()
 	if err != nil {
 		return nil, fmt.Errorf("failed to create AWS session: %v", err)
 	}
 
-	config.S3Svc = s3.New(sess)
+	config.S3Svc = s3SVC
 	config.Sess = sess
 
 	if os.Getenv("S3_BUCKET") == "" {
@@ -58,4 +57,48 @@ func (bh *BlobHandler) PingWithAuth(c echo.Context) error {
 	}
 	log.Infof("Ping operation preformed succesfully, connection to `%s` is healthy", bh.Bucket)
 	return c.JSON(http.StatusOK, "connection is healthy to s3 bucket")
+}
+
+func SessionManager() (*s3.S3, *session.Session, error) {
+	region := os.Getenv("AWS_REGION")
+	accessKeyID := os.Getenv("AWS_ACCESS_KEY_ID")
+
+	//extracts the bool string from env, if not set it will default to false
+	s3MockStr := os.Getenv("S3_MOCK")
+	s3Mock := s3MockStr == "true"
+
+	if s3Mock {
+		minioSecretAccessKey := os.Getenv("MINIO_SECRET_ACCESS_KEY")
+		fmt.Println("Using minio to mock s3")
+		endpoint := os.Getenv("MINIO_S3_ENDPOINT")
+		if endpoint == "" {
+			return nil, nil, errors.New("`MINIO_S3_ENDPOINT` env var required if using Minio (S3_MOCK). Set `S3_MOCK` to false or add an `MINIO_S3_ENDPOINT` to the env")
+		}
+
+		sess, err := session.NewSession(&aws.Config{
+			Endpoint:         aws.String(endpoint),
+			Region:           aws.String(region),
+			Credentials:      credentials.NewStaticCredentials(accessKeyID, minioSecretAccessKey, ""),
+			S3ForcePathStyle: aws.Bool(true),
+		})
+		if err != nil {
+			return nil, nil, fmt.Errorf("error connecting to minio session: %s", err.Error())
+		}
+
+		return s3.New(sess), sess, nil
+
+	} else {
+		awsSecretAccessKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
+		fmt.Println("Using AWS S3")
+		sess, err := session.NewSession(&aws.Config{
+			Region:      aws.String(region),
+			Credentials: credentials.NewStaticCredentials(accessKeyID, awsSecretAccessKey, ""),
+		})
+		if err != nil {
+			return nil, nil, fmt.Errorf("error creating s3 session: %s", err.Error())
+		}
+
+		return s3.New(sess), sess, nil
+
+	}
 }
